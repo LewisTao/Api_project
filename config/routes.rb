6cc1225ab54bb3e9d1f1be0cb15a779264d90121
LewisTao <huytaoquang@gmail.com>
2015-04-28 21:48:15 +0700

    Add update action to the user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 require 'api_constraints'
 
 ApiProject::Application.routes.draw do
-  devise_for :users
   namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/'  do
     scope module: :v1, constraints: ApiConstraints.new(version:1, default: false) do
-    	resources :users, only: [:show]
+    	resources :users, only: [:show, :create, :update]
     end
   end
+  devise_for :users
 end
